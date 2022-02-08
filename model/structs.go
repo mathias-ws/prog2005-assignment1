a8ba527545c0b55480f361f36ba9cadae12fc66b
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-08 13:26:56 +0100

    Set correct data types in structs.

--- model/structs.go
+++ model/structs.go
@@ -11,15 +11,15 @@ type UniversityInfo struct {
 // CountryApi defines the fields taken from the countries api.
 type CountryApi struct {
 	Maps      map[string]string `json:"maps"`
-	Languages []string          `json:"languages"`
+	Languages map[string]string `json:"languages"`
 }
 
 // University Struct that defines the fields of the response given by the api.
 type University struct {
-	Name      string   `json:"name"`
-	Country   string   `json:"country"`
-	Isocode   string   `json:"isocode"`
-	Webpages  []string `json:"webpages"`
-	Languages []string `json:"languages"`
-	Map       string   `json:"map"`
+	Name      string            `json:"name"`
+	Country   string            `json:"country"`
+	Isocode   string            `json:"isocode"`
+	Webpages  []string          `json:"webpages"`
+	Languages map[string]string `json:"languages"`
+	Map       string            `json:"map"`
 }
