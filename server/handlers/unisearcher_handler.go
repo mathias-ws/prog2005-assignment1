75ab5acbc78548ae0c5dfdf881f985509ba7ef2a
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-08 13:53:35 +0100

    Added search for country and name in url and passing it to the api.

--- server/handlers/unisearcher_handler.go
+++ server/handlers/unisearcher_handler.go
@@ -6,6 +6,7 @@ import (
 	"assignment-1/jsonparser"
 	"assignment-1/model"
 	"net/http"
+	"net/url"
 )
 
 // combineStructs combines the university info with the country info and returns the data struct.
@@ -37,8 +38,23 @@ func combine(list []model.UniversityInfo) []model.University {
 	return combinedUniversityList
 }
 
+// urlHandler Gets the search string from the user
+func urlHandler(url *url.URL) string {
+	searchStrings := url.Query()
+
+	urlToSearch := constants.UNIVERSITY_API
+
+	if searchStrings.Has("name") {
+		urlToSearch += "name=" + searchStrings["name"][0]
+	}
+	if searchStrings.Has("country") {
+		urlToSearch += "&country=" + searchStrings["country"][0]
+	}
+	return urlToSearch
+}
+
 // UnisearchHandler is the handler called from the web server.
 func UnisearchHandler(w http.ResponseWriter, r *http.Request) {
 	jsonparser.EncodeUni(w, combine(jsonparser.DecodeUniInfo(
-		client.GetResponseFromWebPage("http://universities.hipolabs.com/search?name=Molde"))))
+		client.GetResponseFromWebPage(urlHandler(r.URL)))))
 }
