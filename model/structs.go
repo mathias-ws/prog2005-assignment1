ca1aff04b564c31122d0f11c54893863bc1839a7
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-08 13:31:44 +0100

    Added borders field.

--- model/structs.go
+++ model/structs.go
@@ -12,6 +12,7 @@ type UniversityInfo struct {
 type CountryApi struct {
 	Maps      map[string]string `json:"maps"`
 	Languages map[string]string `json:"languages"`
+	BordersTo []string          `json:"borders"`
 }
 
 // University Struct that defines the fields of the response given by the api.
