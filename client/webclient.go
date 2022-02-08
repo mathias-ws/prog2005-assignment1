5099c052c4ef9963f3cc5993d24a155a8dc80a78
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-08 09:59:36 +0100

    Implemented a generic web client.

--- client/webclient.go
+++ client/webclient.go
@@ -1,1 +1,32 @@
 package client
+
+import (
+	"fmt"
+	"net/http"
+)
+
+//TODO: Send http error når ingenting matcher søket
+
+// GetResponseFromWebPage method that takes an url and gets a json response from the webpage.
+func GetResponseFromWebPage(url string) *http.Response {
+	request, errorFromRequest := http.NewRequest(http.MethodGet, url, nil)
+
+	if errorFromRequest != nil {
+		fmt.Errorf("Error when creating the request:", errorFromRequest.Error())
+	}
+
+	// Setting the content type header
+	request.Header.Add("content-type", "application/json")
+
+	// Instantiate the webClient
+	webClient := &http.Client{}
+
+	// Sending the request
+	response, errorFromResponse := webClient.Do(request)
+
+	if errorFromResponse != nil {
+		fmt.Errorf("Error in the response:", errorFromResponse.Error())
+	}
+
+	return response
+}
