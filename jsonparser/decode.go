033ad6d0dd355ed686807c0a17dcb0b1e64d2b04
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-08 13:27:47 +0100

    Updated decoder to handle both university info and country info.

--- jsonparser/decode.go
+++ jsonparser/decode.go
@@ -9,10 +9,22 @@ import (
 
 //TODO: Verify valid response?
 
-// DecodeUniInfoIn function that takes a http request and decodes the json body.
-func DecodeUniInfoIn(httpResponse *http.Response) []model.UniFromApi {
+// DecodeUniInfo function that takes a http request and decodes the json body.
+func DecodeUniInfo(httpResponse *http.Response) []model.UniversityInfo {
 	decoder := json.NewDecoder(httpResponse.Body)
-	var list []model.UniFromApi
+	var list []model.UniversityInfo
+
+	if err := decoder.Decode(&list); err != nil {
+		log.Fatal(err)
+	}
+
+	return list
+}
+
+// DecodeCountryInfo function that takes a http request and decodes the json body.
+func DecodeCountryInfo(httpResponse *http.Response) []model.CountryApi {
+	decoder := json.NewDecoder(httpResponse.Body)
+	var list []model.CountryApi
 
 	if err := decoder.Decode(&list); err != nil {
 		log.Fatal(err)
