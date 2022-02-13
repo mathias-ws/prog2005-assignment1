80fc772bba6aa4a4a8602ebd22b8331abf8eb12f
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-13 15:44:08 +0100

    Added some consts for the neighbour unis endpoint.

--- constants/constants.go
+++ constants/constants.go
@@ -1,7 +1,13 @@
 package constants
 
 const PORT = "80"
+
+// URL paths
 const UNISEARCH_LOCATION = "/unisearcher/v1/uniinfo/"
-const LINEBREAK = "\n"
+const NEIGHBOUR_UNIS_LOCATION = "/unisearcher/v1/neighbourunis/"
+const DIAG_LOCATION = "/unisearcher/v1/diag/"
+
+// API urls
 const COUNTRY_API = "https://restcountries.com/v3.1/name/"
+const COUNTRY_API_ALPHA_CODE = "https://restcountries.com/v3.1/alpha/"
 const UNIVERSITY_API = "http://universities.hipolabs.com/search?"
