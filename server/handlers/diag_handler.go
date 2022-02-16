fe21f0f63f59b124df5c9bda4880e1b18fa5bc8c
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-16 19:24:38 +0100

    Added check for rest method. Now serving a http error when using unsupported methods.

--- server/handlers/diag_handler.go
+++ server/handlers/diag_handler.go
@@ -8,5 +8,17 @@ import (
 
 // DiagHandler gets the diag info struct and encodes it.
 func DiagHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		handleGetRequestDiag(w)
+	default:
+		// Returns method not supported for unsupported rest methods.
+		http.Error(w, "Method not supported.", http.StatusMethodNotAllowed)
+	}
+
+}
+
+// handleGetRequestDiag handles the get requests for the endpoint.
+func handleGetRequestDiag(w http.ResponseWriter) {
 	jsonparser.Encode(w, model_logic.GetDiagInfo())
 }
