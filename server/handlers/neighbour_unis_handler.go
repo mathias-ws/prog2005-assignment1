af088dfe1e8240a5a9e723aac6cbfcbf5033b10f
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-16 19:23:06 +0100

    Added check for rest method. Now serving a http error when using unsupported methods.

--- server/handlers/neighbour_unis_handler.go
+++ server/handlers/neighbour_unis_handler.go
@@ -7,7 +7,18 @@ import (
 	"net/http"
 )
 
-// NeighbourUnisHandler handler for the neighbouruni endpoint.
+// NeighbourUnisHandler handler for the neighbouruni endpoint that checks for supported methods.
 func NeighbourUnisHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		handleGetRequestNeighbourUnis(w, r)
+	default:
+		// Returns method not supported for unsupported rest methods.
+		http.Error(w, "Method not supported.", http.StatusMethodNotAllowed)
+	}
+}
+
+// handleGetRequestNeighbourUnis handles the get requests for the endpoint.
+func handleGetRequestNeighbourUnis(w http.ResponseWriter, r *http.Request) {
 	jsonparser.Encode(w, model_logic.GetUniversitiesBorderingTo(url.GetNameAndCountry(r.URL)))
 }
--- server/handlers/unisearcher_handler.go
+++ server/handlers/unisearcher_handler.go
@@ -8,8 +8,19 @@ import (
 	"net/http"
 )
 
-// UnisearchHandler is the handler called from the web server.
+// UnisearchHandler is the handler for the unisearch endpoint that checks for supported methods.
 func UnisearchHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		handleGetRequestUniSearch(w, r)
+	default:
+		// Returns method not supported for unsupported rest methods.
+		http.Error(w, "Method not supported.", http.StatusMethodNotAllowed)
+	}
+}
+
+// handleGetRequestUniSearch handles the get requests for the endpoint.
+func handleGetRequestUniSearch(w http.ResponseWriter, r *http.Request) {
 	jsonparser.Encode(w, model_logic.Combine(jsonparser.DecodeUniInfo(
 		client.GetResponseFromWebPage(url.GenerateUniversitySearchString(r.URL)))))
 }
