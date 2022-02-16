50f49ce9ce306cb39cfbd50d2a5344652a22a9c5
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-16 20:47:46 +0100

    Using string.Builder instead of +

--- server/url/url_handling.go
+++ server/url/url_handling.go
@@ -15,22 +15,25 @@ func GetNameAndCountry(url *url.URL) (string, string) {
 // GenerateUniversitySearchString generates a search string for the university api based on the user inputted url.
 func GenerateUniversitySearchString(url *url.URL) string {
 	searchStrings := url.Query()
-	urlToSearch := constants.UNIVERSITY_API
+	urlTest := strings.Builder{}
+	urlTest.WriteString(constants.UNIVERSITY_API)
 
 	if searchStrings.Has(constants.URL_PARAM_NAME) {
-		urlToSearch += constants.URL_PARAM_NAME + constants.URL_PARAM_EQUALS +
-			strings.ReplaceAll(searchStrings[constants.URL_PARAM_NAME][0], " ", "%20")
+		urlTest.WriteString(constants.URL_PARAM_NAME)
+		urlTest.WriteString(constants.URL_PARAM_EQUALS)
+		urlTest.WriteString(strings.ReplaceAll(searchStrings[constants.URL_PARAM_NAME][0], " ", "%20"))
 	}
+
 	if searchStrings.Has(constants.URL_PARAM_COUNTRY) {
 		// Adds an and if the name parameter is used as well.
-		if strings.Contains(urlToSearch, constants.URL_PARAM_NAME) {
-			urlToSearch += constants.URL_PARAM_AND
+		if strings.Contains(urlTest.String(), constants.URL_PARAM_NAME) {
+			urlTest.WriteString(constants.URL_PARAM_AND)
 		}
 
-		urlToSearch += constants.URL_PARAM_COUNTRY + constants.URL_PARAM_EQUALS +
-			strings.ReplaceAll(searchStrings[constants.URL_PARAM_COUNTRY][0], " ", "%20")
-
+		urlTest.WriteString(constants.URL_PARAM_COUNTRY)
+		urlTest.WriteString(constants.URL_PARAM_EQUALS)
+		urlTest.WriteString(strings.ReplaceAll(searchStrings[constants.URL_PARAM_COUNTRY][0], " ", "%20"))
 	}
 
-	return urlToSearch
+	return urlTest.String()
 }
