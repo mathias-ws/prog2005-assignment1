bef0e8c1de4599c535f7049527391de53f57de45
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-16 19:11:57 +0100

    Made sure that whitespaces are handled correctly.

--- server/url/url_handling.go
+++ server/url/url_handling.go
@@ -3,6 +3,7 @@ package url
 import (
 	"assignment-1/constants"
 	"net/url"
+	"strings"
 )
 
 // GetNameAndCountry Gets the search string from the user containing uni name and country.
@@ -14,14 +15,13 @@ func GetNameAndCountry(url *url.URL) (string, string) {
 // GenerateUniversitySearchString generates a search string for the university api based on the user inputted url.
 func GenerateUniversitySearchString(url *url.URL) string {
 	searchStrings := url.Query()
-
 	urlToSearch := constants.UNIVERSITY_API
 
 	if searchStrings.Has("name") {
-		urlToSearch += "name=" + searchStrings["name"][0]
+		urlToSearch += "name=" + strings.ReplaceAll(searchStrings["name"][0], " ", "%20")
 	}
 	if searchStrings.Has("country") {
-		urlToSearch += "&country=" + searchStrings["country"][0]
+		urlToSearch += "&country=" + strings.ReplaceAll(searchStrings["country"][0], " ", "%20")
 	}
 
 	return urlToSearch
