9ae83e15b503d70dcc148a562302d42ceec3476e
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-16 15:11:40 +0100

    Changed the data type of the values in the Name map.

--- model/structs.go
+++ model/structs.go
@@ -10,10 +10,10 @@ type UniversityInfo struct {
 
 // CountryApi defines the fields taken from the countries api.
 type CountryApi struct {
-	Name      map[string]string `json:"name"`
-	Maps      map[string]string `json:"maps"`
-	Languages map[string]string `json:"languages"`
-	BordersTo []string          `json:"borders"`
+	Name      map[string]interface{} `json:"name"`
+	Maps      map[string]string      `json:"maps"`
+	Languages map[string]string      `json:"languages"`
+	BordersTo []string               `json:"borders"`
 }
 
 // University Struct that defines the fields of the response given by the api.
