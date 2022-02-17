8b5ac194a1aca3560280b98cf19762d04e40f200
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-17 20:54:24 +0100

    Refactored to use more descriptive variable names.

--- model_logic/combine_structs.go
+++ model_logic/combine_structs.go
@@ -54,10 +54,11 @@ func GetUniversitiesBorderingTo(universityName string, searchCountry string) []m
 // Combine takes a slice of university info and combines every element with its country info and appends it to
 //a new slice. The combined slice is returned. The method caches the result from the countries api to minimize
 //the number of requests.
-func Combine(list []model.UniversityInfo) []model.University {
+func Combine(universities []model.UniversityInfo) []model.University {
 	var combinedUniversityList []model.University
 	var countriesCache = map[string]model.CountryApi{}
-	for _, obtainedUniversity := range list {
+
+	for _, obtainedUniversity := range universities {
 		var country model.CountryApi
 		countryName := obtainedUniversity.Country
 
