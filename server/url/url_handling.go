f8118185af4403a34efddbfd64cb538b77101c87
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-17 20:24:04 +0100

    Improved variable name.

--- server/url/url_handling.go
+++ server/url/url_handling.go
@@ -25,25 +25,25 @@ func GetNameAndCountry(url *url.URL) (string, string, error) {
 // GenerateUniversitySearchString generates a search string for the university api based on the user inputted url.
 func GenerateUniversitySearchString(url *url.URL) string {
 	searchStrings := url.Query()
-	urlTest := strings.Builder{}
-	urlTest.WriteString(constants.UNIVERSITY_API)
+	urlToSearch := strings.Builder{}
+	urlToSearch.WriteString(constants.UNIVERSITY_API)
 
 	if searchStrings.Has(constants.URL_PARAM_NAME) {
-		urlTest.WriteString(constants.URL_PARAM_NAME)
-		urlTest.WriteString(constants.URL_PARAM_EQUALS)
-		urlTest.WriteString(strings.ReplaceAll(searchStrings[constants.URL_PARAM_NAME][0], " ", "%20"))
+		urlToSearch.WriteString(constants.URL_PARAM_NAME)
+		urlToSearch.WriteString(constants.URL_PARAM_EQUALS)
+		urlToSearch.WriteString(strings.ReplaceAll(searchStrings[constants.URL_PARAM_NAME][0], " ", "%20"))
 	}
 
 	if searchStrings.Has(constants.URL_PARAM_COUNTRY) {
 		// Adds an and if the name parameter is used as well.
-		if strings.Contains(urlTest.String(), constants.URL_PARAM_NAME) {
-			urlTest.WriteString(constants.URL_PARAM_AND)
+		if strings.Contains(urlToSearch.String(), constants.URL_PARAM_NAME) {
+			urlToSearch.WriteString(constants.URL_PARAM_AND)
 		}
 
-		urlTest.WriteString(constants.URL_PARAM_COUNTRY)
-		urlTest.WriteString(constants.URL_PARAM_EQUALS)
-		urlTest.WriteString(strings.ReplaceAll(searchStrings[constants.URL_PARAM_COUNTRY][0], " ", "%20"))
+		urlToSearch.WriteString(constants.URL_PARAM_COUNTRY)
+		urlToSearch.WriteString(constants.URL_PARAM_EQUALS)
+		urlToSearch.WriteString(strings.ReplaceAll(searchStrings[constants.URL_PARAM_COUNTRY][0], " ", "%20"))
 	}
 
-	return urlTest.String()
+	return urlToSearch.String()
 }
