1caaa9db8942e6e2ba479c92cf4837eb47601dac
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-22 18:06:40 +0100

    Improved the documentation and change into more descriptive variable names.

--- json_parser/decode.go
+++ json_parser/decode.go
@@ -8,11 +8,12 @@ import (
 	"net/http"
 )
 
-// DecodeUniInfo function that takes a http request and decodes the json body.
+// DecodeUniInfo function that takes a http request and decodes the json body into a slice UniversityInfo structs.
 func DecodeUniInfo(httpResponse *http.Response) []model.UniversityInfo {
 	decoder := json.NewDecoder(httpResponse.Body)
 	var list []model.UniversityInfo
 
+	// Checks for errors in the decoding process.
 	if err := decoder.Decode(&list); err != nil {
 		log.Fatal(err)
 	}
@@ -20,11 +21,11 @@ func DecodeUniInfo(httpResponse *http.Response) []model.UniversityInfo {
 	return list
 }
 
-// DecodeCountryInfo function that takes a http request and decodes the json body.
+// DecodeCountryInfo function that takes a http request and decodes the json body into a slice of CountryApi structs.
 // The checking for list or object is inspired from this stack overflow comment:
 // https://stackoverflow.com/a/61837617
 func DecodeCountryInfo(httpResponse *http.Response) []model.CountryApi {
-	var p []byte
+	var responseBytes []byte
 	var list []model.CountryApi
 	var countryObject model.CountryApi
 
@@ -34,18 +35,22 @@ func DecodeCountryInfo(httpResponse *http.Response) []model.CountryApi {
 	}
 
 	// Turns the response body into a byte array
-	p, err := io.ReadAll(httpResponse.Body)
+	responseBytes, err := io.ReadAll(httpResponse.Body)
 	if err != nil {
+		log.Println(err)
 		return nil
 	}
 
 	// Checks if the response is a list or an object and handles them accordingly.
-	if p[0] == '[' {
-		if err := json.Unmarshal(p, &list); err != nil {
+	// This is needed because the country api might return an object when searching for alpha codes with constraints.
+	if responseBytes[0] == '[' {
+		// When the byte array starts with a [ it means that the country api has returned a list.
+		if err := json.Unmarshal(responseBytes, &list); err != nil {
 			log.Println(err)
 		}
-	} else if p[0] == '{' {
-		if err := json.Unmarshal(p, &countryObject); err != nil {
+	} else if responseBytes[0] == '{' {
+		// When the byte array starts with a { it means that the country api has returned an object.
+		if err := json.Unmarshal(responseBytes, &countryObject); err != nil {
 			log.Println(err)
 		}
 		list = append(list, countryObject)
