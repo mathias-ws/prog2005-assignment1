0ae07b768b690d7cbfc68885a4c393625ea3aba6
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-02-22 20:14:37 +0100

    Refactoring and added more documentation in the code.

--- web_server/handlers/diag_handler.go
+++ web_server/handlers/diag_handler.go
@@ -1,29 +1,31 @@
 package handlers
 
 import (
+	"assignment-1/custom_errors"
 	"assignment-1/json_parser"
 	"assignment-1/model_logic"
 	"net/http"
 )
 
-// DiagHandler gets the diag info struct and encodes it.
+// DiagHandler checks for the http method and handles the error appropriately.
 func DiagHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		handleGetRequestDiag(w)
 	default:
 		// Returns method not supported for unsupported rest methods.
-		http.Error(w, "Method not supported.", http.StatusMethodNotAllowed)
+		custom_errors.HttpUnsupportedMethod(w)
 	}
 
 }
 
-// handleGetRequestDiag handles the get requests for the endpoint.
+// handleGetRequestDiag handles the get requests for the endpoint. Gets the diag info struct and encodes it.
 func handleGetRequestDiag(w http.ResponseWriter) {
 	err := json_parser.Encode(w, model_logic.GetDiagInfo())
 
+	// Checks for errors in the encoding process.
 	if err != nil {
-		http.Error(w, "Server side error, please try again later", http.StatusInternalServerError)
+		custom_errors.HttpUnknownServerError(w)
 		return
 	}
 }
